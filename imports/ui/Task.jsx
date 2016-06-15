385125d6c412169d0c6e8aeccfb035cd1648a573
bgroveben <bgroveben@gmail.com>
2016-06-15 06:17:58 -0700

    Create Task component

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+// Task component -- represents an single todo item
+export default class Task extends Component {
+  render() {
+    return (
+      <li>{ this.props.task.text }</li>
+    );
+  }
+}
+
+Task.propTypes = {
+  // This component gets the task to display through a React prop
+  // We can use propTypes to indicate that it is required
+  task: PropTypes.object.isRequired,
+};
