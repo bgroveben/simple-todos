6382aac1eece8b184cd5dd71a4086fd41bbedeeb
bgroveben <bgroveben@gmail.com>
2016-06-17 00:33:42 -0700

    Import accounts configuration

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
+// import accounts-ui configuration code into our client side entrypoint
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
