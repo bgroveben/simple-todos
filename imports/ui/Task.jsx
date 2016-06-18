af128cbb0b0cd026c9317d01fe3e771db1508046
bgroveben <bgroveben@gmail.com>
2016-06-18 01:34:57 -0700

    Bugfixin -- remove comment that was visible on the page

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -29,7 +29,6 @@ export default class Task extends Component {
           checked={this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
-      // add private button that is only available to owner
       { this.props.showPrivateButton ? (
         <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
           { this.props.task.private ? 'Private' : 'Public' }
